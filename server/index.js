b4ee1dc6de25153a1dd026cf7d741c1b9d43a3ac
justinvos <justinvosnz@gmail.com>
2017-07-29 14:40:52 +1200

    Added listening debug log

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const server = require('./server')
 
 const PORT = process.env.PORT || 3000
 
+console.log("start listening")
 server.listen(PORT, function () {
   console.log('Listening on port', PORT)
 })
